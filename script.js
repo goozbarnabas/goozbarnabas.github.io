0c066c9c3ffffa3335d39fe78dc632183445f77d
goozbarnabas <admin@goozbarnabas.ga>
2020-07-03 23:13:42 +0200

    Update 0.1.3
    Added navigation bar.
    
    Added new sites.
    
    Added logo.

--- script.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-        document.getElementById("menubar1").style.width = "100%";
-      }
-      
-      function closeNav() {
-        document.getElementById("menubar1").style.width = "0%";
-      }
